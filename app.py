fbbe1e2e95ad9fe50a99112e2ec96c198001b251
Apefff <102753548+Apefff@users.noreply.github.com>
2026-07-05 13:01:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ api_key = st.text_input("Enter your Google API Key", type="password")
 
 if api_key:
     genai.configure(api_key=api_key)
-    model = genai.generativeai.GenerativeModel('gemini-1.5-flash')
+    model = genai.GenerativeModel('gemini-1.5-flash')
     user_input = st.text_input("สั่งงาน JARVIS:")
     if user_input:
         response = model.generate_content(user_input)
